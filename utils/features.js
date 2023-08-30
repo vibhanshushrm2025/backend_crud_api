131e430aad4ba6316e77049c089bbb9bd4d796d9
vibhanshu2025 <sharmavibhanshu80@gmail.com>
2023-08-30 17:09:51 +0530

    Extended the cookie expiring time from 10 sec to 5 min

--- utils/features.js
+++ utils/features.js
@@ -4,7 +4,7 @@ export const setCookie=(uss,statusCode,message,result,res)=>{
   console.log(token);
   res.cookie("token", token, {
     httpOnly: true,
-    expires: new Date(Date.now() + 10 * 1000),
+    expires: new Date(Date.now() + 300 * 1000),
     sameSite:process.env.NODE_ENV==="development"?"lax":"none",
     secure :process.env.NODE_ENV==="development"?false:true // Above two properties has to be different on deploying , so .dotenv is used 
   });
